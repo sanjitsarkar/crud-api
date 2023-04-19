a4072eefcc4a39b76458d6d77b6b793a73a31d62
HyreKaro <ssanjit856@gmail.com>
2023-04-19 12:50:50 +0530

    fix: fixed the errorHandler

--- src/api/middlewares/errorHandler.ts
+++ src/api/middlewares/errorHandler.ts
@@ -1,6 +1,6 @@
 import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
 
-export const errorHandler: ErrorRequestHandler = (err: Error, _: Request, res: Response, __: NextFunction) => {
-  return res.status(500).send('Somethig went wrong')
+export const errorHandler: ErrorRequestHandler = (error: Error, _: Request, res: Response, next: NextFunction) => {
+  return res.status(500).send(error?.message || 'Something went wrong.')
 }
 
